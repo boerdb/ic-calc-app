3721c85510bec072c79163959a4dfe8b0747989c
boerdb <75579276+boerdb@users.noreply.github.com>
2025-12-27 19:32:56 +0100

    Versie 1.0 compleet: Tab 2 en 3 layout gelijkgetrokken en ScvO2 toegevoegd

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,12 +7,15 @@ import {
   IonContent, IonHeader, IonToolbar, IonTitle,
   IonList, IonItem, IonInput, IonButton,
   IonCard, IonCardHeader, IonCardTitle, IonCardContent,
-  IonLabel, IonNote, IonGrid, IonRow, IonCol, IonButtons } from '@ionic/angular/standalone';
+  IonLabel, IonNote, IonGrid, IonRow, IonCol, IonButtons,
+  IonBadge,AlertController, IonIcon // <--- TOEGEVOEGD
+} from '@ionic/angular/standalone';
 
 // Onze eigen services
 import { CalculatorService } from '../services/calculator';
 import { PatientService } from '../services/patient';
-
+import { addIcons } from 'ionicons';
+import { informationCircleOutline } from 'ionicons/icons'; // <--- TOEGEVOEGD
 @Component({
   selector: 'app-tab2',
   templateUrl: './tab2.page.html',
@@ -23,13 +26,13 @@ import { PatientService } from '../services/patient';
     IonContent, IonHeader, IonToolbar, IonTitle,
     IonList, IonItem, IonInput, IonButton,
     IonCard, IonCardHeader, IonCardTitle, IonCardContent,
-    IonLabel, IonNote, IonGrid, IonRow, IonCol
+    IonLabel, IonNote, IonGrid, IonRow, IonCol,
+    IonBadge,IonIcon // <--- TOEGEVOEGD AAN IMPORTS
   ]
 })
 export class Tab2Page {
 
   // --- INPUTS ---
-  // We gebruiken korte namen die matchen met de HTML uit de vorige stap
   fio2: number | null = null;    // %
   pao2: number | null = null;    // kPa
   paco2: number | null = null;   // kPa
@@ -46,7 +49,7 @@ export class Tab2Page {
   resPFRatio: number | null = null;
   resCaO2: number | null = null;
 
-  // --- STATUS TEKSTEN & KLEUREN (Jouw logica) ---
+  // --- STATUS TEKSTEN & KLEUREN ---
   aaStatusTekst = '';
   aaStatusKleur = 'medium';
   aaVerwacht: number | null = null;
@@ -54,13 +57,38 @@ export class Tab2Page {
   pfStatusTekst = '';
   pfStatusKleur = 'medium';
 
+  // NIEUW: SvO2 Status variabelen
+  resSvO2Message = '';
+  resSvO2Color = 'medium';
+
   // Helper om te zien of we resultaten moeten tonen
   toonResultaten = false;
 
   constructor(
     public patient: PatientService, // Voor de leeftijd (patient.current.leeftijd)
-    private calc: CalculatorService // Voor de formules
-  ) {}
+    private calc: CalculatorService, // Voor de formules
+    private alertCtrl: AlertController // <--- TOEGEVOEGD
+  ) {
+    addIcons({
+      informationCircleOutline
+    });
+  }
+
+// --- AANGEPASTE INFO FUNCTIE (Nu zonder HTML tags) ---
+  async toonInfo() {
+    const alert = await this.alertCtrl.create({
+      header: 'Parameter Info',
+      // We gebruiken \n voor een nieuwe regel. Dit werkt altijd veilig.
+      message:
+        'PAO₂: Zuurstofspanning in de longblaasjes (Alveolair).\n\n' +
+        'A-a Gradiënt: Het drukverschil tussen longblaasje en bloed. Maat voor diffusie.\n\n' +
+        'P/F Ratio (Horowitz): Maat voor de ernst van longschade (ARDS).\n\n' +
+        'CaO₂: Totale zuurstofinhoud in het arteriële bloed (gebonden + opgelost).\n\n' +
+        'ScvO₂: Balans tussen zuurstofaanbod en zuurstofverbruik.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 
   bereken() {
     // Minimale vereisten: FiO2 en PaCO2
@@ -77,7 +105,6 @@ export class Tab2Page {
       this.resAaRatio = this.calc.calcAaRatio(this.pao2, this.resPAO2);
 
       // --- JOUW LOGICA: Verwachte Gradiënt op basis van leeftijd ---
-      // We pakken de leeftijd nu uit de PatientService
       const leeftijd = this.patient.current.leeftijd || 20; // Default 20 als niet ingevuld
       this.aaVerwacht = 2.0 + (leeftijd * 0.03);
 
@@ -90,7 +117,6 @@ export class Tab2Page {
       }
 
       // --- JOUW LOGICA: P/F Ratio (Horowitz) ---
-      // FiO2 moet als fractie (0.5) voor deze som, input is % (50)
       const fiO2Decimaal = this.fio2 / 100;
       this.resPFRatio = this.pao2 / fiO2Decimaal;
 
@@ -102,7 +128,7 @@ export class Tab2Page {
         this.pfStatusKleur = 'warning';
       } else if (this.resPFRatio > 13.3) {
         this.pfStatusTekst = 'Matige ARDS';
-        this.pfStatusKleur = 'warning'; // Of orange als je die class hebt
+        this.pfStatusKleur = 'warning';
       } else {
         this.pfStatusTekst = 'Ernstige ARDS';
         this.pfStatusKleur = 'danger';
@@ -112,6 +138,23 @@ export class Tab2Page {
     // 3. Oxygen Content (CaO2) - Alleen als Hb en SaO2 er zijn
     if (this.hb != null && this.sao2 != null && this.pao2 != null) {
       this.resCaO2 = this.calc.calcCaO2(this.hb, this.sao2, this.pao2);
+    }
+
+    // 4. NIEUW: SvO2 / ScvO2 Interpretatie
+    if (this.svo2 != null) {
+      if (this.svo2 < 60) {
+        this.resSvO2Color = 'danger';
+        this.resSvO2Message = 'Laag! Verhoogde extractie (DD: Laag HMV/Shock, Laag Hb, Pijn/Koorts)';
+      } else if (this.svo2 > 80) {
+        this.resSvO2Color = 'warning';
+        this.resSvO2Message = 'Hoog! Verlaagde extractie (DD: Sepsis/Shunting, Leverfalen)';
+      } else {
+        this.resSvO2Color = 'success';
+        this.resSvO2Message = 'Normaal (Balans DO2/VO2 adequaat)';
+      }
+    } else {
+      // Als veld leeg is, ook bericht leegmaken
+      this.resSvO2Message = '';
     }
 
     this.toonResultaten = true;
@@ -127,8 +170,13 @@ export class Tab2Page {
 
     this.resPAO2 = null;
     this.resAaGrad = null;
+    this.resAaRatio = null; // Ook even resetten
     this.resPFRatio = null;
     this.resCaO2 = null;
+
+    // SvO2 resetten
+    this.resSvO2Message = '';
+    this.resSvO2Color = 'medium';
 
     this.toonResultaten = false;
   }
